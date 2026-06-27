193437a14fa8973dcaad8e97027aa357df7cd755
harshpandey-2025 <harsh.pandey2025@glbajajgroup.org>
2026-06-27 10:51:34 +0530

    Day-19 assignment is uploaded

---
+++ Day-19/Subtract_matrices.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+int main() {
+    int rows, cols;
+    int a[100][100], b[100][100], diff[100][100];
+    int i, j;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    printf("Enter elements of first matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Enter elements of second matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            diff[i][j] = a[i][j] - b[i][j];
+        }
+    }
+
+    printf("Difference of matrices:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("%d ", diff[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ Day-19/Transpose_matrix.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+    int a[100][100], transpose[100][100];
+    int i, j;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    printf("Enter elements of the matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Find transpose
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            transpose[j][i] = a[i][j];
+        }
+    }
+
+    printf("Transpose of the matrix:\n");
+    for (i = 0; i < cols; i++) {
+        for (j = 0; j < rows; j++) {
+            printf("%d ", transpose[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
